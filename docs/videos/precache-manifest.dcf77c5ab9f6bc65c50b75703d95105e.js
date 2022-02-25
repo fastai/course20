08ed17680cc579f7797fac25d89dc5d03db66eae
Jeremy Howard <j@fast.ai>
2022-02-25 11:43:32 +1000

    build

---
+++ docs/videos/precache-manifest.dcf77c5ab9f6bc65c50b75703d95105e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a235b5a6ebbba5fed167",
+    "url": "/static/css/main.57635021.chunk.css"
+  },
+  {
+    "revision": "a235b5a6ebbba5fed167",
+    "url": "/static/js/main.34fca4ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b11c0e9d0bd8ddca3409",
+    "url": "/static/css/2.634556e2.chunk.css"
+  },
+  {
+    "revision": "b11c0e9d0bd8ddca3409",
+    "url": "/static/js/2.a526cada.chunk.js"
+  },
+  {
+    "revision": "dbf5b9b7cb8194fdc61d74e19688e2db",
+    "url": "/index.html"
+  }
+];
